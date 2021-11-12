44d39e38dd9835f50c3e3a0b4fafb472eb632b17
Jay Kumar <jaykumar094@outlook.com>
2021-11-11 21:56:03 -0800

    added wasm version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React from 'react'
 import './App.css';
 import Portfolio from './components/Portfolio';
 
-function App() {
-  return (
-    <div className="container">
-      <Portfolio></Portfolio>
-    </div>
-  );
+export default class App extends React.Component {
+  componentDidMount() {
+    WebAssembly.instantiateStreaming(fetch("golib.wasm"), go.importObject).then(async (result) => {
+      go.run(result.instance)
+    });
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <Portfolio />
+      </div>
+    )
+  }
 }
-
-export default App;
